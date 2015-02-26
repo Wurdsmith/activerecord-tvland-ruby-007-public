4727fea23fcdaef9c3dbea3a179ed8f295053ea0
deniznida <denizunat@hotmail.com>
2015-02-26 08:42:18 -0500

    done

--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -1,36 +1,51 @@
 require_relative 'spec_helper'
 
 describe "Actor" do
-  #TODO: implement the tests as described in the it blocks,
-  #      and implement the class and migrations required to pass them
-
-  # HINTS: look at show_spec.rb and network_spec.rb and character_spec.rb for guidance
-
-  xit "has a first and last name" do
-    # TODO set up the basic data model for actor
+#TODO: implement the tests as described in the it blocks,
+# and implement the class and migrations required to pass them
+# HINTS: look at show_spec.rb and network_spec.rb and character_spec.rb for guidance
+  it "has a first and last name" do
+    actor = Actor.create(:first_name => "Patrick", :last_name => "Stewart")
+    expect(actor.first_name).to eq("Patrick")
+    expect(actor.last_name).to eq("Stewart")
   end
 
-  xit "has associated characters in an array" do
-    # Hint: think about what migration you'll need to write so that an actor can have many characters.
-    # Where will the association foreign key go?
-    # TODO set up the appropriate association for characters and add characters through the array push
+  it "has associated characters in an array" do
+    george = Actor.create(first_name: "George", last_name: "Georges")
+    characters = [
+      Character.new(:name => "Samson"),
+      Character.new(:name => "Humperdink")
+    ]
+    george.characters << characters
+    george.save
+    expect(george.characters.count).to eq(2)
   end
 
-  xit "can build its associated characters" do
-    # TODO add a character to an actor with a build method
+  it "can build its associated characters" do
+    jam = Character.new(:name => "jamgirl")
+    jam.build_show(:name => "i like jam and stuff")
+    expect(jam.show.name).to eq("i like jam and stuff")
   end
 
-  xit "can build its associated shows through its characters" do
-    # TODO in one line, build a show and a character for this actor
+  it "can build its associated shows through its characters" do
+  # TODO in one line, build a show and a character for this actor
+    george = Actor.create(first_name: "George", last_name: "Georges")
+    george.characters.build(name: "Georgie Poo").build_show(name: "Georgie Poo's Show")
+    character = george.characters[0]
+    expect(character.name).to eq("Georgie Poo")
+    expect(character.show.name).to eq("Georgie Poo's Show")
   end
 
-  xit "can list its full name" do
-    # TODO create an instance method on actor called .full_name to return first and last name together
+  it "can list its full name" do
+  # TODO create an instance method on actor called .full_name to return first and last name together
+    george = Actor.create(first_name: "George", last_name: "Georges")
+    expect(george.full_name).to eq("George Georges")
   end
 
-  xit "can list all of its shows and characters" do
-    # TODO create a list_roles method
-    # TODO: build a method on actor that will return a string in the form of
-    # character name - show name\n and test the results
+  it "can list all of its shows and characters" do
+  # TODO create a list_roles method
+    george = Actor.create(first_name: "George", last_name: "Georges")
+    george.characters.build(name: "Georgie Poo").build_show(name: "Georgie Poo's Show")
+    expect(george.list_roles).to include("Georgie Poo - Georgie Poo's Show")
   end
 end
